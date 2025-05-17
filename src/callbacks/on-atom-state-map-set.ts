9b5b48651bd3abd7b375873333fa874ff1ec2f1f
Wendy FOUCHTER <wendy.fouchter@gmail.com>
2025-05-17 15:14:34 +0200

    refactor: use Reflect.set in atom state proxy
    - Use Reflect.set instead of changing the atom state manually to prevent
      future bugs.
    - Also remove unused code when initializing atom's state since Jotai
      never initialize an atom state with a value.

--- src/callbacks/on-atom-state-map-set.ts
+++ src/callbacks/on-atom-state-map-set.ts
@@ -3,26 +3,14 @@ import { type INTERNAL_AtomState } from 'jotai/vanilla/internals';
 
 import { ATOMS_LOGGER_SYMBOL } from '../consts/atom-logger-symbol.js';
 import type { StoreWithAtomsLogger } from '../types/atoms-logger.js';
-import { getInternalBuildingBlocks } from '../utils/get-internal-building-blocks.js';
 import { shouldShowAtom } from '../utils/should-show-atom.js';
 import { onAtomValueChanged } from './on-atom-value-changed.js';
 
 export function getOnAtomStateMapSet(store: StoreWithAtomsLogger) {
   return function onAtomStateMapSet(atom: Atom<unknown>, atomState: INTERNAL_AtomState): void {
-    const { atomStateMap } = getInternalBuildingBlocks(store);
+    let isInitialValue = true;
 
-    let isInitialValue = !atomStateMap.get(atom);
-
-    if ('v' in atomState) {
-      const oldState = atomStateMap.get(atom);
-      onAtomValueChanged(store, atom, {
-        isInitialValue: !oldState,
-        oldValue: oldState?.v,
-        newValue: atomState.v,
-      });
-    }
-
-    if (shouldShowAtom(store, atom) && !atomStateMap.get(atom)) {
+    if (shouldShowAtom(store, atom)) {
       store[ATOMS_LOGGER_SYMBOL].atomsFinalizationRegistry.register(atom, atom.toString());
     }
 
@@ -31,15 +19,10 @@ export function getOnAtomStateMapSet(store: StoreWithAtomsLogger) {
         const prop = _prop as keyof typeof target;
         if (prop === 'v') {
           const oldValue = target[prop];
-          onAtomValueChanged(store, atom, {
-            isInitialValue,
-            oldValue,
-            newValue,
-          });
+          onAtomValueChanged(store, atom, { isInitialValue, oldValue, newValue });
           isInitialValue = false;
         }
-        (target as unknown as Record<string, unknown>)[prop] = newValue;
-        return true;
+        return Reflect.set(target, prop, newValue);
       },
     });
 
